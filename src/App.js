2547ca0e751aec22e9c2a6419a21260f2dfbc9cc
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-02-06 01:20:55 +0900

    3rd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TopPage from "./pages/TopPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>Good morning</h1> */}
+      <TopPage />
     </div>
   );
 }
---
+++ src/pages/TopPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../components/Title";
+
+const TopPage = () => {
+  return (
+    <div>
+      <Title />
+    </div>
+  );
+};
+
+export default TopPage;
