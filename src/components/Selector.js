d32fe513a4b644675a331974507ca77a51c54cb9
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-02-07 20:10:06 +0900

    aa

---
+++ src/components/Selector.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Selector = () => {
+  return (
+    <div>
+      <select>
+        <option>USA</option>
+        <option>JAPAN</option>
+        <option>CHINA</option>
+      </select>
+    </div>
+  );
+};
+
+export default Selector;
--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Title from "../components/Title";
+import Selector from "../components/Selector";
 
 const TopPage = () => {
   return (
     <div>
       <Title />
+      <Selector />
     </div>
   );
 };
