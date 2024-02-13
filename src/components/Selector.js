75f159d277d58b2c06ce7c851490fdb74e18ba6f
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-02-13 17:46:38 +0900

    test

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -4,7 +4,10 @@ import countriesJson from "../country.json";
 const Selector = () => {
   return (
     <div>
-      <select>{countriesJson.map((country) => console.log(country))}</select>
+      <select>
+        {countriesJson.map((country) => console.log(country.Country))}
+        {/* 国データの読み込み */}
+      </select>
     </div>
   );
 };
